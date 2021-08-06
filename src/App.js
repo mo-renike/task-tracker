777db2ee739126aec3fc60ad328825fe7cb17484
Morenike Oyewole <herroyalpianist@gmail.com>
2021-08-06 08:46:25 +0100

    Update Tasks.app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-
+import Tasks from "./components/Tasks"
 
 
 function App() {
   return (
     <div className="container">
       <Header/>
+      <Tasks />
     </div>
   );
 }
